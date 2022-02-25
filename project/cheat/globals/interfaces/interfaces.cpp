189732cbc3f26d3fe2dd4c98e589a45831855760
Liga <liga@wednesday.wtf>
2022-02-25 13:43:48 -0800

    Fix interfaces.cpp and something in cheat.cpp

--- project/cheat/globals/interfaces/interfaces.cpp
+++ project/cheat/globals/interfaces/interfaces.cpp
@@ -3,31 +3,31 @@
 void interfaces::impl::init( )
 {
 	std::vector< const char* > needed_modules = {
-//		"client.dll",
+		//		"client.dll",
 		"engine.dll",
-//		"localize.dll",
-//		"materialsystem.dll",
-//		"vguimatsurface.dll",
-//		"vgui2.dll",
-//		"shaderapidx9.dll",
-//		"gameoverlayrenderer.dll",
-//		"vphysics.dll",
-//		"vstdlib.dll",
-//		"tier0.dll",
-//		"inputsystem.dll",
-//		"studiorender.dll",
-//		"datacache.dll",
-//		"steam_api.dll",
-//		"matchmaking.dll",
-//		"server.dll",
-//		"serverbrowser.dll",
-//		"filesystem_stdio.dll",
+		//		"localize.dll",
+		//		"materialsystem.dll",
+		//		"vguimatsurface.dll",
+		//		"vgui2.dll",
+		//		"shaderapidx9.dll",
+		//		"gameoverlayrenderer.dll",
+		//		"vphysics.dll",
+		//		"vstdlib.dll",
+		//		"tier0.dll",
+		//		"inputsystem.dll",
+		//		"studiorender.dll",
+		//		"datacache.dll",
+		//		"steam_api.dll",
+		//		"matchmaking.dll",
+		//		"server.dll",
+		//		"serverbrowser.dll",
+		//		"filesystem_stdio.dll",
 	};
 
 	for ( auto& module : needed_modules ) {
 		auto create_interface = reinterpret_cast< std::uintptr_t >( get_proc_address( get_module_handle( module ), "CreateInterface" ) );
 
-		while ( *reinterpret_cast< unsigned char* >( create_interface ) != 0xE9 )
+		while ( *reinterpret_cast< std::uint8_t* >( create_interface ) != 0xE9 )
 			++create_interface;
 
 		create_interface = jump_address( create_interface );
@@ -40,7 +40,7 @@ void interfaces::impl::init( )
 		char buffer_name[ 128 ]{ };
 
 		while ( node ) {
-			std::size_t size{};
+			std::size_t size{ };
 
 			for ( size = 0; node->name[ size ] != '\0'; ++size )
 				buffer_name[ size ] = node->name[ size ];
@@ -56,15 +56,12 @@ void interfaces::impl::init( )
 
 void interfaces::impl::shutdown( ) { }
 
-
-
-
 std::uintptr_t interfaces::impl::jump_address( std::uintptr_t address )
 {
 	auto jump_relative = *reinterpret_cast< std::uintptr_t* >( address + 0x1 );
 
-	jump_relative += 0x5;
-	jump_relative += jump_relative;
+	address += 0x5;
+	address += jump_relative;
 
 	return address;
 }
