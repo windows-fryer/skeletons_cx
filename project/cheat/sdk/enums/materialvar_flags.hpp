83f45844ba420979a97f8bbb9690f786d268bc2c
blanket <fuckblanket@protonmail.com>
2022-02-25 17:40:58 -0500

    new enums

---
+++ project/cheat/sdk/enums/materialvar_flags.hpp
@@ -0,0 +1,45 @@
+#ifndef SKELETONS_CX_MATERIALVAR_FLAGS_HPP
+#define SKELETONS_CX_MATERIALVAR_FLAGS_HPP
+
+
+namespace sdk
+{
+	enum material_var_flags
+	{
+		material_var_debug                    = ( 1 << 0 ),
+		material_var_no_debug_override        = ( 1 << 1 ),
+		material_var_no_draw                  = ( 1 << 2 ),
+		material_var_use_in_fillrate_mode     = ( 1 << 3 ),
+		material_var_vertexcolor              = ( 1 << 4 ),
+		material_var_vertexalpha              = ( 1 << 5 ),
+		material_var_selfillum                = ( 1 << 6 ),
+		material_var_additive                 = ( 1 << 7 ),
+		material_var_alphatest                = ( 1 << 8 ),
+		material_var_multipass                = ( 1 << 9 ),
+		material_var_znearer                  = ( 1 << 10 ),
+		material_var_model                    = ( 1 << 11 ),
+		material_var_flat                     = ( 1 << 12 ),
+		material_var_nocull                   = ( 1 << 13 ),
+		material_var_nofog                    = ( 1 << 14 ),
+		material_var_ignorez                  = ( 1 << 15 ),
+		material_var_decal                    = ( 1 << 16 ),
+		material_var_envmapsphere             = ( 1 << 17 ),
+		material_var_noalphamod               = ( 1 << 18 ),
+		material_var_envmapcameraspace        = ( 1 << 19 ),
+		material_var_basealphaenvmapmask      = ( 1 << 20 ),
+		material_var_translucent              = ( 1 << 21 ),
+		material_var_normalmapalphaenvmapmask = ( 1 << 22 ),
+		material_var_needs_software_skinning  = ( 1 << 23 ),
+		material_var_opaquetexture            = ( 1 << 24 ),
+		material_var_envmapmode               = ( 1 << 25 ),
+		material_var_suppress_decals          = ( 1 << 26 ),
+		material_var_halflambert              = ( 1 << 27 ),
+		material_var_wireframe                = ( 1 << 28 ),
+		material_var_allowalphatocoverage     = ( 1 << 29 ),
+		material_var_alpha_modified_by_proxy  = ( 1 << 30 ),
+		material_var_vertexfog                = ( 1 << 31 )
+	};
+} // namespace sdk
+
+
+#endif // SKELETONS_CX_MATERIALVAR_FLAGS_HPP
