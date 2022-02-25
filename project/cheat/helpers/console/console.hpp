4d61700f291ad4290e711e3d5801d899714724e6
Liga <liga@wednesday.wtf>
2022-02-25 14:39:32 -0800

    Added some stuff

--- project/cheat/helpers/console/console.hpp
+++ project/cheat/helpers/console/console.hpp
@@ -7,23 +7,16 @@
 
 namespace console
 {
+	void init( );
+	void shutdown( );
 
-	struct impl {
-	public:
-		void init( );
-		void shutdown( );
+	template< typename... ARGS >
+	void log( std::string_view message, ARGS&&... arguments )
+	{
+		std::string_view formatted_string = std::vformat( message, std::make_format_args( arguments... ) );
 
-		template< typename... ARGS >
-		void log( std::string_view message, ARGS&&... arguments )
-		{
-			std::string_view formatted_string = std::vformat( message, std::make_format_args( arguments... ) );
-
-			std::cout << formatted_string;
-		}
-	};
-
+		std::cout << formatted_string;
+	}
 } // namespace console
-
-inline console::impl g_console;
 
 #endif // SKELETONS_CX_CONSOLE_HPP
--- project/cheat/helpers/hooking/hooking.hpp
+++ project/cheat/helpers/hooking/hooking.hpp
@@ -1,11 +1,32 @@
 #ifndef SKELETONS_CX_HOOKING_HPP
 #define SKELETONS_CX_HOOKING_HPP
 
+#include <iostream>
+
+#include "../../../dependencies/minhook/MinHook.h"
+#include "../console/console.hpp"
+
 namespace hooking
 {
+	template< class T >
+	struct hook_helper {
+	private:
+		void* source;
+		void* original;
+
+	public:
+		hook_helper( auto source, auto destination, const char* name = "undefined" )
+		{
+			if ( MH_CreateHook( ( void* )source, ( void* )destination, &original ) != MH_OK )
+				g_console.log( "Failed to create hook [{}]", name );
+
+
+		}
+	};
+
 	struct impl {
 	public:
-
+		std::vector <
 	};
 } // namespace hooking
 
