6ec8e2bc32d1e653a8145b5b78693068328a3373
Liga <liga@wednesday.wtf>
2022-02-25 14:25:58 -0800

    Added minhook

---
+++ project/cheat/helpers/hooking/hooking.hpp
@@ -0,0 +1,12 @@
+#ifndef SKELETONS_CX_HOOKING_HPP
+#define SKELETONS_CX_HOOKING_HPP
+
+namespace hooking
+{
+	struct impl {
+	public:
+
+	};
+} // namespace hooking
+
+#endif // SKELETONS_CX_HOOKING_HPP
