e2c565a2581117d875068bb8de399df4213c3fee
blanket <fuckblanket@protonmail.com>
2022-02-25 19:12:13 -0500

    patternscan

--- project/cheat/helpers/modules/modules.hpp
+++ project/cheat/helpers/modules/modules.hpp
@@ -2,6 +2,8 @@
 #define SKELETONS_CX_MODULES_HPP
 
 #include "../../globals/snakeify.hpp"
+#include "../patternscan/patternscan.hpp"
+
 #include <Windows.h>
 #include <string>
 
@@ -20,6 +22,13 @@ namespace modules
 			// ASSERT( module_handle )
 		}
 
+		patternscan::address pattern_scan(std::string_view sig){
+			const auto module_address   = reinterpret_cast< std::uint8_t* >( module_handle );
+			const auto dos_header       = reinterpret_cast< PIMAGE_DOS_HEADER >( module_handle );
+			const auto nt_headers       = reinterpret_cast< PIMAGE_NT_HEADERS >( module_address + dos_header->e_lfanew );
+			
+			return g_patternscan.find_pattern( module_address, nt_headers->OptionalHeader.SizeOfImage, sig.data());
+		}
 		std::string get_module_name( )
 		{
 			return module_name; // copy is slow.. too bad!
---
+++ project/cheat/helpers/patternscan/patternscan.hpp
@@ -0,0 +1,147 @@
+#ifndef SKELETONS_CX_PATTERNSCAN_HPP
+#define SKELETONS_CX_PATTERNSCAN_HPP
+#include <iostream>
+#include <vector>
+
+namespace patternscan
+{
+	struct address {
+	protected:
+		std::uintptr_t m_address{ };
+
+	public:
+		constexpr address( ) : m_address{ 0 } { };
+
+		address( std::uintptr_t ad ) : m_address{ ad } { };
+
+		address( void* address ) : m_address{ reinterpret_cast< std::uintptr_t >( address ) } { };
+
+		operator std::uintptr_t( ) const
+		{
+			return m_address;
+		}
+
+		operator void*( ) const
+		{
+			return reinterpret_cast< void* >( m_address );
+		}
+
+		template< typename T = address >
+		T to( ) const
+		{
+			return *( T* )( m_address );
+		}
+
+		template< typename T = address >
+		T as( ) const
+		{
+			return m_address ? ( T )( m_address ) : T( );
+		}
+
+		template< typename T = address >
+		T at( std::ptrdiff_t offset ) const
+		{
+			return m_address ? *( T* )( m_address + offset ) : T( );
+		}
+
+		template< typename T = address >
+		T add( std::ptrdiff_t offset ) const
+		{
+			return m_address ? ( T )( m_address + offset ) : T( );
+		}
+
+		template< typename T = address >
+		T sub( std::ptrdiff_t offset ) const
+		{
+			return m_address ? ( T )( m_address - offset ) : T( );
+		}
+
+		template< typename T = address >
+		T get( std::uint8_t dereferences = 1 ) const
+		{
+			if ( !m_address )
+				return T( );
+
+			auto addr = m_address;
+
+			while ( dereferences-- )
+				if ( addr )
+					addr = *reinterpret_cast< std::uintptr_t* >( addr );
+
+			return ( T )( addr );
+		}
+
+		template< typename T = std::uintptr_t >
+		void set( const T& value )
+		{
+			if ( !m_address )
+				return;
+
+			*( T* )( m_address ) = value;
+		}
+
+		template< typename T = address >
+		T relative( std::ptrdiff_t offset = 0x1 ) const
+		{
+			if ( !m_address )
+				return T( );
+
+			const std::uintptr_t new_address = m_address + offset;
+
+			const auto relative_offset = *reinterpret_cast< std::int32_t* >( new_address );
+			if ( !relative_offset )
+				return T( );
+
+			return ( T )( new_address + sizeof( std::int32_t ) + relative_offset );
+		}
+	};
+
+
+	struct impl {
+	public:
+		std::uintptr_t find_pattern( std::uint8_t* region_start, std::uintptr_t region_size, const char* pattern )
+		{
+			std::vector< int > vec_bytes = pattern_to_bytes( pattern );
+			for ( unsigned long i = 0UL; i < region_size - vec_bytes.size( ); ++i ) {
+				bool byte_found = true;
+				for ( unsigned long s = 0UL; s < vec_bytes.size( ); ++s ) {
+					if ( region_start[ i + s ] != vec_bytes[ s ] && vec_bytes[ s ] != -1 ) {
+						byte_found = false;
+						break;
+					}
+				}
+
+				if ( byte_found )
+					return reinterpret_cast< std::uintptr_t >( &region_start[ i ] );
+			}
+			return 0U;
+		}
+
+		std::vector< int > pattern_to_bytes( const char* pattern )
+		{
+			std::vector< int > vec_bytes = { };
+			char* start                  = const_cast< char* >( pattern );
+			char* end                    = start + strlen( pattern );
+
+			for ( char* current = start; current < end; ++current ) {
+				if ( *current == '?' ) {
+					++current;
+
+					if ( *current == '?' )
+						++current;
+
+					vec_bytes.push_back( -1 );
+				} else
+					// convert byte to hex
+					vec_bytes.push_back( strtoul( current, &current, 16 ) );
+			}
+
+			return vec_bytes;
+		}
+	};
+
+} // namespace patternscan
+
+inline patternscan::impl g_patternscan;
+
+#endif // SKELETONS_CX_PATTERNSCAN_HPP
